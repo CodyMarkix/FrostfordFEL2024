91422424896cb006fa6f123faa9c277b98d890ed
CodyMarkix <marekplasek08@protonmail.com>
2024-11-19 00:04:25 +0100

    holy shit I may have it
    but idrk, we're gonna have to test it on-site

--- src/collection.py
+++ src/collection.py
@@ -45,56 +45,74 @@ class Collection:
         # accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(32), block=True)
         # self.backpack[1].update({"color": "blue", "count": self.backpack[1]["count"] + 1})
 
-        steer.on_for_degrees(SpeedRPM(200), 90, block=True)
-        time.sleep(0.25)
+        iter = 0
+        while iter < 4:
+            if iter != 0:
+                accel.on_for_rotations(SpeedRPM(120), (self.brick.cmToRotations(28) * -1), block=True)
+                time.sleep(0.5)
+                
+                steer.on_for_degrees(SpeedRPM(200), 90, block=True)
+                time.sleep(0.25)
 
-        accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(18), block=True)
-        self.map.moveByPosition((1, 0))
-        self.map.moveByPosition((0, -1))
+                accel.on_for_rotations(SpeedRPM(160), (self.brick.cmToRotations(18) * - 1), block=True)
+                self.map.moveByPosition((-1, 0))
+                self.map.moveByPosition((0, 1))
+                time.sleep(0.5)
 
-        steer.on_for_degrees(SpeedRPM(200), -90, block=True)
-        self.backpack[1].update({"color": "blue", "count": self.backpack[0]["count"] + 1})
-        
-        time.sleep(0.5) # Should now be at y: 1; x: 2
+                steer.on_for_degrees(SpeedRPM(200), -90, block=True)
+
+                accel.on_for_rotations(SpeedRPM(160), (self.brick.cmToRotations(83 - 28 * iter) * - 1), block=True)
+
+            steer.on_for_degrees(SpeedRPM(200), 90, block=True)
+            time.sleep(0.25)
+
+            accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(18), block=True)
+            self.map.moveByPosition((1, 0))
+            self.map.moveByPosition((0, -1))
+
+            steer.on_for_degrees(SpeedRPM(200), -90, block=True)
+            self.backpack[1].update({"color": "blue", "count": self.backpack[0]["count"] + 1})
+            
+            time.sleep(0.5) # Should now be at y: 1; x: 2
 
 
-        accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(56), block=True)
-        self.backpack[1].update({"color": "blue", "count": self.backpack[0]["count"] + 1})
-        self.backpack[0].update({"color": "red", "count": self.backpack[1]["count"] + 1})
-        self.map.moveByPosition((0, -2))
+            accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(56), block=True)
+            self.backpack[1].update({"color": "blue", "count": self.backpack[0]["count"] + 1})
+            self.backpack[0].update({"color": "red", "count": self.backpack[1]["count"] + 1})
+            self.map.moveByPosition((0, -2))
 
-        time.sleep(0.5) # Should now be at y: 1; x: 0
+            time.sleep(0.5) # Should now be at y: 1; x: 0
 
-        cradle.on_for_degrees(60, 60, block=True)
-        cradle.stop_action = 'hold'
-        cradle.stop()
+            cradle.on_for_degrees(60, 60, block=True)
+            cradle.stop_action = 'hold'
+            cradle.stop()
 
-        accel.on_for_rotations(SpeedRPM(120), (self.brick.cmToRotations(28) * -1), block=True)
+            accel.on_for_rotations(SpeedRPM(120), (self.brick.cmToRotations(28) * -1), block=True)
 
-        time.sleep(0.5) # Should now be at y: 1; x: 2
+            time.sleep(0.5) # Should now be at y: 1; x: 2
 
-        steer.on_for_degrees(SpeedRPM(200), 90, block=True)
-        accel.on_for_rotations(SpeedRPM(160), (self.brick.cmToRotations(18) * - 1), block=True)
-        self.map.moveByPosition((-1, 0))
-        self.map.moveByPosition((0, 1))
+            steer.on_for_degrees(SpeedRPM(200), 90, block=True)
+            accel.on_for_rotations(SpeedRPM(160), (self.brick.cmToRotations(18) * - 1), block=True)
+            self.map.moveByPosition((-1, 0))
+            self.map.moveByPosition((0, 1))
 
-        time.sleep(0.5) # Should now be at y: 0; x: 3
+            time.sleep(0.5) # Should now be at y: 0; x: 3
 
-        steer.on_for_degrees(SpeedRPM(200), -90, block=True)
-        accel.on_for_rotations(SpeedRPM(160), self.brick.cmToRotations(112), block=True)
-        self.map.moveByPosition((0, 4))
-        
-        time.sleep(0.5)
+            steer.on_for_degrees(SpeedRPM(200), -90, block=True)
+            accel.on_for_rotations(SpeedRPM(160), self.brick.cmToRotations(112), block=True)
+            self.map.moveByPosition((0, 4))
+            
+            time.sleep(0.5)
 
-        steer.on_for_degrees(SpeedRPM(200), 90, block=True)
-        time.sleep(0.25)
+            steer.on_for_degrees(SpeedRPM(200), 90, block=True)
+            time.sleep(0.25)
 
-        accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(18), block=True)
-        self.map.moveByPosition((1, 0))
-        self.map.moveByPosition((0, -1)) # Should now be at: y: 5; x: 2
+            accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(18), block=True)
+            self.map.moveByPosition((1, 0))
+            self.map.moveByPosition((0, -1)) # Should now be at: y: 5; x: 2
 
-        steer.on_for_degrees(SpeedRPM(200), -90, block=True)
-        accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(14), block=True)
-        self.map.moveByPosition((0, 1)) # Should now be at y: 5; x: 1
+            steer.on_for_degrees(SpeedRPM(200), -90, block=True)
+            accel.on_for_rotations(SpeedRPM(120), self.brick.cmToRotations(14), block=True)
+            self.map.moveByPosition((0, 1)) # Should now be at y: 5; x: 1
 
-        cradle.on_for_degrees(60, 20)
+            cradle.on_for_degrees(60, 20)
